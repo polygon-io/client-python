52b61106e1dade12d3043b852fee05056d5dc60e
jbonzo <8647805+jbonzo@users.noreply.github.com>
2019-11-06 15:27:12 -0500

    Adding swagger generated code

---
+++ polygon/rest/api/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from .crypto_api import CryptoApi
+from .forex__currencies_api import ForexCurrenciesApi
+from .reference_api import ReferenceApi
+from .stocks__equities_api import StocksEquitiesApi
