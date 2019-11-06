1228ccb157cb5873a023bc502658e1a4fb22bdea
jbonzo <8647805+jbonzo@users.noreply.github.com>
2019-11-06 15:27:44 -0500

    Removing swagger codegen files

--- polygon/rest/api/__init__.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-
-# flake8: noqa
-
-# import apis into api package
-from polygon.rest.api.crypto_api import CryptoApi
-from polygon.rest.api.forex__currencies_api import ForexCurrenciesApi
-from polygon.rest.api.reference_api import ReferenceApi
-from polygon.rest.api.stocks__equities_api import StocksEquitiesApi
