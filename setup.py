d45f43d6ebffacf660edae3eabdb1f05cce5efce
jbonzo <8647805+jbonzo@users.noreply.github.com>
2020-01-06 14:31:21 -0500

    add long description to pypi page

--- setup.py
+++ setup.py
@@ -10,12 +10,20 @@ if not version:
     print("no version supplied")
     sys.exit(1)
 
+def get_readme_md_contents():
+    """read the contents of your README file"""
+    with open("README.md", encoding='utf-8') as f:
+        long_description = f.read()
+        return long_description
+
 setup(
     name="polygon-api-client",
     version=version,
     description="Polygon API client",
-    author_email="ricky@polygon.io",
-    url="https://github.com/Polygon-io/client-python",
+    long_description=get_readme_md_contents(),
+    long_description_content_type="text/markdown",
+    author_email="support@polygon.io",
+    url="https://github.com/polygon-io/client-python",
     packages=find_packages(),
     classifiers=[
         "License :: OSI Approved :: MIT License",
