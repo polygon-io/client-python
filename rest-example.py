2e8c76cb9f091899c55692ad745ba3a2ef81a0d2
Ricky Barillas <8647805+jbonzo@users.noreply.github.com>
2020-08-25 11:28:13 -0400

    Make RESTful client a context manager and update example code (#37)

--- rest-example.py
+++ rest-example.py
@@ -3,10 +3,12 @@ from polygon import RESTClient
 
 def main():
     key = "your api key"
-    client = RESTClient(key)
 
-    resp = client.stocks_equities_daily_open_close("AAPL", "2018-03-02")
-    print(f"On: {resp.from_} Apple opened at {resp.open} and closed at {resp.close}")
+    # RESTClient can be used as a context manager to facilitate closing the underlying http session
+    # https://requests.readthedocs.io/en/master/user/advanced/#session-objects
+    with RESTClient(key) as client:
+        resp = client.stocks_equities_daily_open_close("AAPL", "2018-03-02")
+        print(f"On: {resp.from_} Apple opened at {resp.open} and closed at {resp.close}")
 
 
 if __name__ == '__main__':
