c8172642080daaaba5de02b6704b3465bf990f90
jbonzo <8647805+jbonzo@users.noreply.github.com>
2021-01-26 15:11:25 -0500

    update readme to look like the example files and vice versa

--- rest-example.py
+++ rest-example.py
@@ -1,4 +1,10 @@
+import datetime
+
 from polygon import RESTClient
+
+
+def ts_to_datetime(ts) -> str:
+    return datetime.datetime.fromtimestamp(ts / 1000.0).strftime('%Y-%m-%d %H:%M')
 
 
 def main():
@@ -7,8 +13,15 @@ def main():
     # RESTClient can be used as a context manager to facilitate closing the underlying http session
     # https://requests.readthedocs.io/en/master/user/advanced/#session-objects
     with RESTClient(key) as client:
-        resp = client.stocks_equities_daily_open_close("AAPL", "2018-03-02")
-        print(f"On: {resp.from_} Apple opened at {resp.open} and closed at {resp.close}")
+        from_ = "2019-01-01"
+        to = "2019-02-01"
+        resp = client.stocks_equities_aggregates("AAPL", 1, "minute", from_, to, unadjusted=False)
+
+        print(f"Minute aggregates for {resp.ticker} between {from_} and {to}.")
+
+        for result in resp.results:
+            dt = ts_to_datetime(result["t"])
+            print(f"{dt}\n\tO: {result['o']}\n\tH: {result['h']}\n\tL: {result['l']}\n\tC: {result['c']} ")
 
 
 if __name__ == '__main__':
