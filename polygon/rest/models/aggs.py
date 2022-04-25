983b5fda8bde8794b9e2555b4f89f6b6a2f36c6c
clickingbuttons <zack@polygon.io>
2022-04-25 11:48:58 -0400

    copy rfc over (#114)

---
+++ polygon/rest/models/aggs.py
@@ -0,0 +1,26 @@
+from dataclasses import dataclass
+
+@dataclass
+class Agg:
+    open: float
+    high: float
+    low: float
+    close: float
+    volume: float
+    vwap: float
+    timestamp: int
+    transactions: int
+
+    @staticmethod
+    def from_dict(d):
+        return Agg(
+            d['o'],
+            d['h'],
+            d['l'],
+            d['c'],
+            d['v'],
+            d['vw'],
+            d['t'],
+            d['n']
+        )
+
