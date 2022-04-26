2a65a1a1161db78185df99edd176f410756c9446
Darcy Linde <47221647+Darcy-Linde@users.noreply.github.com>
2022-04-26 14:06:07 -0400

    Markets (#121)

--- polygon/rest/__init__.py
+++ polygon/rest/__init__.py
@@ -1,7 +1,8 @@
 from .aggs import AggsClient
 from .trades import TradesClient
 from .quotes import QuotesClient
+from .reference import MarketsClient
 
 
-class RESTClient(AggsClient, TradesClient, QuotesClient):
+class RESTClient(AggsClient, TradesClient, QuotesClient, MarketsClient):
     pass
---
+++ polygon/rest/models/markets.py
@@ -0,0 +1,31 @@
+from typing import Optional, Dict
+from dataclasses import dataclass
+
+
+@dataclass
+class MarketHoliday:
+    "MarketHoliday contains data for upcoming market holidays and their open/close times."
+    close: Optional[str] = None
+    date: Optional[str] = None
+    exchange: Optional[str] = None
+    name: Optional[str] = None
+    open: Optional[str] = None
+    status: Optional[str] = None
+
+    @staticmethod
+    def from_dict(d):
+        return MarketHoliday(**d)
+
+@dataclass
+class MarketStatus:
+    "MarketStatus contains data for the current trading status of the exchanges and overall financial markets."
+    after_hours: Optional[bool] = None
+    currencies: Optional[Dict[str, str]] = None
+    early_hours: Optional[bool] = None
+    exchanges: Optional[Dict[str, str]] = None
+    market: Optional[str] = None
+    server_time: Optional[str] = None
+
+    @staticmethod
+    def from_dict(d):
+        return MarketStatus(**d)
