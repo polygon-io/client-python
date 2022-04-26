d12de64ddeb7b17ce94263332ef1d06329df7d1e
Darcy Linde <{47221647+Darcy-Linde@users.noreply.github.com}>
2022-04-26 11:17:55 -0400

    add quotes import

--- polygon/rest/__init__.py
+++ polygon/rest/__init__.py
@@ -1,6 +1,7 @@
 from .aggs import AggsClient
 from .trades import TradesClient
+from .quotes import QuotesClient
 
 
-class RESTClient(AggsClient, TradesClient):
+class RESTClient(AggsClient, TradesClient, QuotesClient):
     pass
