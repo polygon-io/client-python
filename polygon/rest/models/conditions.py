2655722735859292f4d8e6f17ff06fe153e8028e
Darcy Linde <{47221647+Darcy-Linde@users.noreply.github.com}>
2022-04-27 15:12:17 -0400

    add Condition dataclass

---
+++ polygon/rest/models/conditions.py
@@ -0,0 +1,28 @@
+from typing import Optional, List
+from . import AssetClass
+from dataclasses import dataclass
+
+
+@dataclass
+class SipMapping:
+    CTA: Optional[str] = None
+    OPRA: Optional[str] = None
+    UTP: Optional[str] = None
+
+
+@dataclass
+class Condition:
+    "Condition contains data for a condition that Polygon.io uses."
+    abbreviation: Optional[str] = None
+    asset_class: Optional[AssetClass] = None
+    data_types: Optional[List[str]] = None
+    description: Optional[str] = None
+    exchange: Optional[int] = None
+    id: Optional[int] = None
+    legacy: Optional[bool] = None
+    name: Optional[str] = None
+    sip_mapping: Optional[SipMapping] = None
+
+    @staticmethod
+    def from_dict(d):
+        return Condition(**d)
