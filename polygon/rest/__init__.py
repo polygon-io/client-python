aa6c84bf9ca778ea08b03edf9fdb74b583b595ed
Darcy Linde <47221647+Darcy-Linde@users.noreply.github.com>
2022-04-27 11:37:22 -0400

    Splits (#124)

--- polygon/rest/__init__.py
+++ polygon/rest/__init__.py
@@ -1,8 +1,10 @@
 from .aggs import AggsClient
 from .trades import TradesClient
 from .quotes import QuotesClient
-from .reference import MarketsClient, TickersClient
+from .reference import MarketsClient, TickersClient, SplitsClient
 
 
-class RESTClient(AggsClient, TradesClient, QuotesClient, MarketsClient, TickersClient):
+class RESTClient(
+    AggsClient, TradesClient, QuotesClient, MarketsClient, TickersClient, SplitsClient
+):
     pass
