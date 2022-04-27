bf3ffb3c8ba1a5b5f02d0ec97196e9aa5d562c3f
Darcy Linde <{47221647+Darcy-Linde@users.noreply.github.com}>
2022-04-27 12:34:37 -0400

    add dividendsclient import

--- polygon/rest/__init__.py
+++ polygon/rest/__init__.py
@@ -1,10 +1,10 @@
 from .aggs import AggsClient
 from .trades import TradesClient
 from .quotes import QuotesClient
-from .reference import MarketsClient, TickersClient, SplitsClient
+from .reference import MarketsClient, TickersClient, SplitsClient, DividendsClient
 
 
 class RESTClient(
-    AggsClient, TradesClient, QuotesClient, MarketsClient, TickersClient, SplitsClient
+    AggsClient, TradesClient, QuotesClient, MarketsClient, TickersClient, SplitsClient, DividendsClient
 ):
     pass
