95534fabc6672acd6a814a264e4e377c2c05f1c2
Darcy Linde <{47221647+Darcy-Linde@users.noreply.github.com}>
2022-04-28 16:00:48 -0400

    add resultskey to get market holidays

--- polygon/rest/reference.py
+++ polygon/rest/reference.py
@@ -23,7 +23,6 @@ from .models import (
 from urllib3 import HTTPResponse
 from datetime import date
 
-# https://polygon.io/docs/stocks
 class MarketsClient(BaseClient):
     def get_market_holidays(
         self, params: Optional[Dict[str, Any]] = None, raw: bool = False
@@ -38,7 +37,7 @@ class MarketsClient(BaseClient):
         url = "/v1/marketstatus/upcoming"
 
         return self._get(
-            path=url, params=params, deserializer=MarketHoliday.from_dict, raw=raw
+            path=url, params=params, deserializer=MarketHoliday.from_dict, raw=raw, result_key="results",
         )
 
     def get_market_status(
