51b780091ca2ed905773797582bd9d3ab1ad40f8
zack <43246297+clickingbuttons@users.noreply.github.com>
2022-05-02 11:32:51 -0400

    fix style

--- tests/base.py
+++ tests/base.py
@@ -7,15 +7,16 @@ import httpretty  # type: ignore
 # mocks are stored in file tree
 mocks = []
 dirname = os.path.dirname(__file__)
-mockdir = os.path.join(dirname, 'mocks')
+mockdir = os.path.join(dirname, "mocks")
 for dname, _, files in os.walk(mockdir):
     for fname in files:
-        if fname.endswith('.json'):
+        if fname.endswith(".json"):
             abspath = os.path.join(dname, fname)
-            with open(abspath, 'r') as f:
-                urllpath = abspath.replace(mockdir, '').replace('.json', '')
+            with open(abspath, "r") as f:
+                urllpath = abspath.replace(mockdir, "").replace(".json", "")
                 mocks.append((urllpath, f.read()))
-                #print('load', urllpath)
+                # print('load', urllpath)
+
 
 class BaseTest(unittest.TestCase):
     @classmethod
@@ -26,6 +27,4 @@ class BaseTest(unittest.TestCase):
         for m in mocks:
             url = cls.c.BASE + m[0]
             # print('register', url)
-            httpretty.register_uri(
-                httpretty.GET, url, m[1], match_querystring=True
-            )
+            httpretty.register_uri(httpretty.GET, url, m[1], match_querystring=True)
