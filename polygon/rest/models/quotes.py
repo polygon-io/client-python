954ab30d2df444dd15a799ecdcece4eda666eab6
Darcy Linde <{47221647+Darcy-Linde@users.noreply.github.com}>
2022-05-10 11:49:25 -0400

    RealTimeCurrencyConversion model

--- polygon/rest/models/quotes.py
+++ polygon/rest/models/quotes.py
@@ -87,3 +87,23 @@ class LastForexQuote:
             last=None if "last" not in d else [Last.from_dict(d["last"])],
             symbol=d.get("symbol", None),
         )
+
+
+@dataclass
+class RealTimeCurrencyConversion:
+    "RealTimeCurrencyConversion contains data for currency conversions using the latest market conversion rates."
+    converted: Optional[float] = None
+    from_: Optional[str] = None
+    initial_amount: Optional[float] = None
+    last: Optional[Last] = None
+    to: Optional[str] = None
+
+    @staticmethod
+    def from_dict(d):
+        return RealTimeCurrencyConversion(
+            converted=d.get("converted", None),
+            from_=d.get("from_", None),
+            initial_amount=d.get("initialAmount", None),
+            last=None if "last" not in d else [Last.from_dict(d["last"])],
+            to=d.get("to", None),
+        )
