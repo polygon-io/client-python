b60282f88ed2bf5f221609037c14d6a7ac0dfad0
Darcy Linde <{47221647+Darcy-Linde@users.noreply.github.com}>
2022-05-10 11:02:14 -0400

    Precision enum class

--- polygon/rest/models/common.py
+++ polygon/rest/models/common.py
@@ -76,3 +76,11 @@ class SnapshotMarketType(Enum):
 class Timeframe(Enum):
     ANNUAL = "annual"
     QUARTERLY = "quarterly"
+
+
+class Precision(Enum):
+    ZERO = 0
+    ONE = 1
+    TWO = 2
+    THREE = 3
+    FOUR = 4
