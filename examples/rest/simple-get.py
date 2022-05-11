2d2d36afd6f9e87611c3a3696c2929480a1e0458
zack <43246297+clickingbuttons@users.noreply.github.com>
2022-05-11 12:40:28 -0400

    remove api key

--- examples/rest/simple-get.py
+++ examples/rest/simple-get.py
@@ -1,6 +1,6 @@
 from polygon import RESTClient
 
-client = RESTClient(api_key="s7Cwsxiaj32BVtjrIvHkVwAiF9kytfyk")
+client = RESTClient()
 
 aggs = client.get_aggs("AAPL", 1, "day", "2004-04-04", "2004-04-04")
 print(aggs)
