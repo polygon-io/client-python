d20b9655cc028cbc44429ce8ca35996ccdf3c736
zack <43246297+clickingbuttons@users.noreply.github.com>
2022-05-11 14:58:15 -0400

    debugging

--- test_rest/base.py
+++ test_rest/base.py
@@ -24,6 +24,7 @@ for dname, _, files in os.walk(mockdir):
                 if ":" in urllpath:
                     raise Exception(f"use ; instead of : in path ${urllpath}")
                 urllpath = urllpath.replace(";", ":", 1)
+                print(urllpath)
                 mocks.append((urllpath, f.read()))
 
 unittest.util._MAX_LENGTH = 30000  # type: ignore
