f96a942cb3bdb1d7fe618030095f9ca1339cef85
Darcy Linde <{47221647+Darcy-Linde@users.noreply.github.com}>
2022-05-11 14:14:08 -0400

    remove enums from exchanges

--- polygon/rest/models/exchanges.py
+++ polygon/rest/models/exchanges.py
@@ -1,5 +1,4 @@
 from typing import Optional
-from .common import AssetClass, Locale, ExchangeType
 from dataclasses import dataclass
 
 
@@ -7,14 +6,14 @@ from dataclasses import dataclass
 class Exchange:
     "Exchange contains data for a condition that Polygon.io uses."
     acronym: Optional[str] = None
-    asset_class: Optional[AssetClass] = None
+    asset_class: Optional[str] = None
     id: Optional[int] = None
-    locale: Optional[Locale] = None
+    locale: Optional[str] = None
     mic: Optional[str] = None
     name: Optional[str] = None
     operating_mic: Optional[str] = None
     participant_id: Optional[str] = None
-    type: Optional[ExchangeType] = None
+    type: Optional[str] = None
     url: Optional[str] = None
 
     @staticmethod
