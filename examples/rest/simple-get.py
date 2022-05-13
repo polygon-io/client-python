ccbeb626b063e9b9d94b28e35cda968b9d3e8e0e
zack <43246297+clickingbuttons@users.noreply.github.com>
2022-05-13 11:29:09 -0400

    bump example date

--- examples/rest/simple-get.py
+++ examples/rest/simple-get.py
@@ -2,5 +2,5 @@ from polygon import RESTClient
 
 client = RESTClient()
 
-aggs = client.get_aggs("AAPL", 1, "day", "2004-04-04", "2004-04-04")
+aggs = client.get_aggs("AAPL", 1, "day", "2022-04-04", "2022-04-04")
 print(aggs)
