f934a1e3fa7736075b74bc170bd6df6ede69e418
Darcy Linde <{47221647+Darcy-Linde@users.noreply.github.com}>
2022-05-16 14:55:28 -0400

    add default values for list_tickers limit, order, and sort

--- polygon/rest/reference.py
+++ polygon/rest/reference.py
@@ -78,9 +78,9 @@ class TickersClient(BaseClient):
         date: Optional[str] = None,
         active: Optional[bool] = None,
         search: Optional[str] = None,
-        limit: Optional[int] = None,
-        sort: Optional[Union[str, Sort]] = None,
-        order: Optional[Union[str, Order]] = None,
+        limit: Optional[int] = 10,
+        sort: Optional[Union[str, Sort]] = "ticker",
+        order: Optional[Union[str, Order]] = "asc",
         params: Optional[Dict[str, Any]] = None,
         raw: bool = False,
     ) -> Union[Iterator[Ticker], HTTPResponse]:
