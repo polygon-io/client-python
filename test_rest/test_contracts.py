3ae594e9fec41817194e77a01efd810cc762327d
Darcy Linde <{47221647+Darcy-Linde@users.noreply.github.com}>
2022-05-17 12:17:30 -0400

    test_get_options_contract

--- test_rest/test_contracts.py
+++ test_rest/test_contracts.py
@@ -5,6 +5,17 @@ from base import BaseTest
 class ContractsTest(BaseTest):
     def test_get_options_contract(self):
         contract = self.c.get_options_contract("OEVRI240119C00002500")
-        expected = [
-        ]
+        expected = OptionsContract(
+            additional_underlyings=None,
+            cfi="OCASPS",
+            correction=None,
+            exercise_style="american",
+            expiration_date="2024-01-19",
+            primary_exchange="BATO",
+            shares_per_contract=100,
+            strike_price=2.5,
+            size=None,
+            ticker="O:EVRI240119C00002500",
+            underlying_ticker="EVRI",
+        )
         self.assertEqual(contract, expected)
