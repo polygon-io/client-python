e90bc5817bf42629ef610a7790d732a7c90cbf25
clickingbuttons <zack@polygon.io>
2022-05-18 13:51:05 -0400

    add module __version__ (#190)

--- polygon/__init__.py
+++ polygon/__init__.py
@@ -1,3 +1,5 @@
 from .rest import RESTClient
-from .rest.base import NoResultsError
+from .rest.base import NoResultsError, version
 from .websocket import WebSocketClient, AuthError
+
+__version__ = version
