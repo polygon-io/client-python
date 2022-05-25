58200205af3cd4ba60fcf23466dc79660ac8a0d2
Darcy Linde <47221647+Darcy-Linde@users.noreply.github.com>
2022-05-25 14:18:10 -0400

    Fix broken from_dict methods (#206)
    * from_dict should use camelCase in these instances
    
    * update tests to reflect from_dict change
    
    Co-authored-by: Darcy Linde <{47221647+Darcy-Linde@users.noreply.github.com}>

--- polygon/rest/models/markets.py
+++ polygon/rest/models/markets.py
@@ -53,14 +53,14 @@ class MarketStatus:
     @staticmethod
     def from_dict(d):
         return MarketStatus(
-            after_hours=d.get("after_hours", None),
+            after_hours=d.get("afterHours", None),
             currencies=None
             if "currencies" not in d
             else MarketCurrencies.from_dict(d["currencies"]),
-            early_hours=d.get("early_hours", None),
+            early_hours=d.get("earlyHours", None),
             exchanges=None
             if "exchanges" not in d
             else MarketExchanges.from_dict(d["exchanges"]),
             market=d.get("market", None),
-            server_time=d.get("server_time", None),
+            server_time=d.get("serverTime", None),
         )
