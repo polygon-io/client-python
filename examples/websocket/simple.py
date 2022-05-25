9a72a4e25377cb31d7125c3a63015a58c615c8a1
zack <43246297+clickingbuttons@users.noreply.github.com>
2022-05-25 16:56:13 -0400

    turn off verbose

--- examples/websocket/simple.py
+++ examples/websocket/simple.py
@@ -2,7 +2,7 @@ from polygon import WebSocketClient
 from polygon.websocket.models import WebSocketMessage
 from typing import List
 
-c = WebSocketClient(subscriptions=["T.*"], verbose=True)
+c = WebSocketClient(subscriptions=["T.*"])
 
 
 def handle_msg(msgs: List[WebSocketMessage]):
