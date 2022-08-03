d9fa54f51e51cd32ceac1fa193fdfbcbe4f0d964
Vera Harless <53271741+morningvera@users.noreply.github.com>
2022-08-03 10:35:56 -0400

    add indicators to ws equity model

--- polygon/websocket/models/models.py
+++ polygon/websocket/models/models.py
@@ -145,6 +145,7 @@ class EquityQuote:
     ask_price: Optional[float] = None
     ask_size: Optional[int] = None
     condition: Optional[int] = None
+    indicators: Optional[List[int]] = None
     timestamp: Optional[int] = None
     tape: Optional[int] = None
     sequence_number: Optional[int] = None
@@ -161,6 +162,7 @@ class EquityQuote:
             d.get("ap", None),
             d.get("as", None),
             d.get("c", None),
+            d.get("i", None),
             d.get("t", None),
             d.get("z", None),
             d.get("q", None),
