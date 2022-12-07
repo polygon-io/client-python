33948a454bdc9da93a7b055a62beb0a2e7a01eb0
chaig15 <7276836+chaig15@users.noreply.github.com>
2022-12-06 23:19:16 -0800

    summaries endpoint framework

---
+++ polygon/rest/models/summaries.py
@@ -0,0 +1,83 @@
+from sqlite3 import Timestamp
+from typing import Optional, Any, Dict, List, Union
+from ...modelclass import modelclass
+from .aggs import Agg
+
+
+@modelclass
+class IndicatorValue:
+    "Contains one datum for indicators with a single value."
+    timestamp: Optional[int] = None
+    value: Optional[float] = None
+
+    @staticmethod
+    def from_dict(d):
+        return IndicatorValue(
+            timestamp=d.get("timestamp", None),
+            value=d.get("value", None),
+        )
+
+
+@modelclass
+class MACDIndicatorValue:
+    "Contains one datum for all MACD values."
+    timestamp: Optional[int] = None
+    value: Optional[float] = None
+    signal: Optional[float] = None
+    histogram: Optional[float] = None
+
+    @staticmethod
+    def from_dict(d):
+        return MACDIndicatorValue(
+            timestamp=d.get("timestamp", None),
+            value=d.get("value", None),
+            signal=d.get("histogram", None),
+            histogram=d.get("signal", None),
+        )
+
+
+@modelclass
+class IndicatorUnderlying:
+    "Contains the URL to call to get the aggs used for building the indicator."
+    url: Optional[str] = None
+    aggregates: Optional[List[Agg]] = None
+
+    @staticmethod
+    def from_dict(d):
+        return IndicatorUnderlying(
+            url=d.get("url", None),
+            aggregates=[Agg.from_dict(a) for a in d.get("aggregates", [])],
+        )
+
+
+@modelclass
+class SingleIndicatorResults:
+    "Contains indicator values and Underlying."
+    values: Optional[List[IndicatorValue]] = None
+    underlying: Optional[IndicatorUnderlying] = None
+
+    @staticmethod
+    def from_dict(d):
+        return SingleIndicatorResults(
+            values=[IndicatorValue.from_dict(v) for v in (d.get("values", []))],
+            underlying=IndicatorUnderlying.from_dict(d.get("underlying", None)),
+        )
+
+
+SMAIndicatorResults = SingleIndicatorResults
+EMAIndicatorResults = SingleIndicatorResults
+RSIIndicatorResults = SingleIndicatorResults
+
+
+@modelclass
+class MACDIndicatorResults:
+    "Contains indicator values and Underlying."
+    values: Optional[List[MACDIndicatorValue]] = None
+    underlying: Optional[IndicatorUnderlying] = None
+
+    @staticmethod
+    def from_dict(d):
+        return MACDIndicatorResults(
+            values=[MACDIndicatorValue.from_dict(v) for v in (d.get("values", []))],
+            underlying=IndicatorUnderlying.from_dict(d.get("underlying", None)),
+        )
---
+++ polygon/rest/summaries.py
@@ -0,0 +1,42 @@
+from polygon.rest.models.common import SeriesType
+from polygon.rest.models.indicators import (
+    SMAIndicatorResults,
+    EMAIndicatorResults,
+    RSIIndicatorResults,
+    MACDIndicatorResults,
+)
+from .base import BaseClient
+from typing import Optional, Any, Dict, List, Union
+from .models import Order
+from urllib3 import HTTPResponse
+from datetime import datetime, date
+
+
+class SummariesClient(BaseClient):
+    def get_summaries(
+        self,
+        ticker_any_of: str,
+        params: Optional[Dict[str, Any]] = None,
+        raw: bool = False,
+    ) -> Union[SMAIndicatorResults, HTTPResponse]:
+        """
+        GetSummaries retrieves summaries for the ticker list with the given params.
+        For more details see https://polygon.io/docs/stocks/get_v1_summaries.
+
+        :param ticker_any_of: The ticker symbol
+        :param params: Any additional query params
+        :param raw: Return raw object instead of results object
+        :return: SummaryResults
+        """
+
+        url = f"/v1/summaries/"
+
+        return self._get(
+            path=url,
+            params=self._get_params(self.get_sma, locals()),
+            result_key="results",
+            deserializer=SMAIndicatorResults.from_dict,
+            raw=raw,
+        )
+
+
