a4500a57a00a4f378bcf6cc50f46953299420668
chaig15 <7276836+chaig15@users.noreply.github.com>
2022-12-07 17:55:21 -0800

    remove test and format json

--- test_rest/test_summaries.py
+++ test_rest/test_summaries.py
@@ -3,37 +3,6 @@ from base import BaseTest
 
 
 class SummariesTest(BaseTest):
-    def test_get_summaries(self):
-        ticker_any_of = ["NCLH"]
-        summary_results = self.c.get_summaries(ticker_any_of)
-        expected = [
-            SummaryResult(
-                price=22.3,
-                name="Norwegian Cruise Lines",
-                ticker="NCLH",
-                branding=Branding(
-                    icon_url="https://api.polygon.io/icon.png",
-                    logo_url="https://api.polygon.io/logo.svg",
-                ),
-                market_status="closed",
-                type="stocks",
-                session=Session(
-                    change=-1.05,
-                    change_percent=-4.67,
-                    close=21.4,
-                    early_trading_change=-0.39,
-                    early_trading_change_percent=-0.07,
-                    high=22.49,
-                    late_trading_change=1.2,
-                    late_trading_change_percent=3.92,
-                    low=21.35,
-                    open=22.49,
-                    previous_close=22.45,
-                    volume=37,
-                ),
-            )
-        ]
-        self.assertEqual(summary_results, expected)
 
     def test_get_summaries_list(self):
         ticker_any_of = ["NCLH", "O:NCLH221014C00005000", "C:EURUSD", "X:BTCUSD", "APx"]
