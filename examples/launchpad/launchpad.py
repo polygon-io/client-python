63885c436cf9533a39cea0a89451711613bfc11b
antdjohns <114414459+antdjohns@users.noreply.github.com>
2022-12-09 09:33:20 -0800

    removed one example from ./example/launchpad and renamed function

--- examples/launchpad/launchpad.py
+++ examples/launchpad/launchpad.py
@@ -2,27 +2,22 @@ from polygon import RESTClient
 from polygon.rest.models.request import RequestOptionBuilder
 
 
-def get_aggs_launchpad():
+def get_list_trades_launchpad():
     client = RESTClient()
 
     """
-    options can be added to the RequestOptionBuilder both directly in 
-    initialization 
-    Example:
-    `options = RequestOptionBuilder(edge_id="", edge_ip_address="")
-    
-    or you can use the builder patten for future modifications to 
-    underlying edge header dictionary.
-    Example:
+    set headers example:
     options = RequestOptionBuilder()
         .edge_headers(edge_id="EDGE_ID", edge_ip_address="EDGE_ID_ADDRESS")
-        .update(edge_id="NEW")
+        
+    update headers example:
     options = options.update_edge_header(edge_ip_address="NEW_IP")
     """
     options = (
         RequestOptionBuilder()
-        .edge_headers(edge_id="EDGE_ID", edge_ip_address="EDGE_ID_ADDRESS")
-        .update_edge_header(edge_user="EDGE_USER")
+        .edge_headers(edge_id="EDGE_ID",
+                      edge_ip_address="EDGE_ID_ADDRESS",
+                      edge_user="EDGE_USER")
     )
 
     trades = []
@@ -32,7 +27,7 @@ def get_aggs_launchpad():
 
 
 def main():
-    get_aggs_launchpad()
+    get_list_trades_launchpad()
 
 
 if __name__ == "__main__":
