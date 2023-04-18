ab7707059363e36411e658886c8535ec244d47fd
justinpolygon <123573436+justinpolygon@users.noreply.github.com>
2023-04-17 20:49:55 -0700

    Add indices.py ws example
    Adding a example Indices example that works for streaming both aggregates and values.

---
+++ examples/websocket/indices.py
@@ -0,0 +1,25 @@
+from polygon import WebSocketClient
+from polygon.websocket.models import WebSocketMessage, Market
+from typing import List
+
+client = WebSocketClient(market=Market.Indices)
+
+# aggregates (per minute)
+#client.subscribe("AM.*") # all aggregates
+client.subscribe("AM.I:SPX") # Standard & Poor's 500
+client.subscribe("AM.I:DJI") # Dow Jones Industrial Average
+client.subscribe("AM.I:NDX") # Nasdaq-100
+client.subscribe("AM.I:VIX") # Volatility Index
+
+# single index
+#client.subscribe("V.I:SPX") # Standard & Poor's 500
+#client.subscribe("V.I:DJI") # Dow Jones Industrial Average
+#client.subscribe("V.I:NDX") # Nasdaq-100
+#client.subscribe("V.I:VIX") # Volatility Index
+#client.subscribe("V.*") # all tickers
+
+def handle_msg(msgs: List[WebSocketMessage]):
+    for m in msgs:
+        print(m)
+
+client.run(handle_msg)
