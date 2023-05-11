e80bb55593caa06938303cd9337a0733f83d16b8
justinpolygon <123573436+justinpolygon@users.noreply.github.com>
2023-05-11 00:56:54 -0700

    Fix lint checks

--- examples/rest/demo_correlation_matrix.py
+++ examples/rest/demo_correlation_matrix.py
@@ -37,28 +37,29 @@ as it can help manage risk and inform investment strategies. It's always
 essential to do your own research or consult a financial advisor for 
 personalized advice when investing.
 """
-import pandas as pd
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
+import pandas as pd # type: ignore
+import numpy as np # type: ignore
+import seaborn as sns # type: ignore
+import matplotlib.pyplot as plt # type: ignore
 from polygon import RESTClient
 
-# Less likely to be correlated due to being in different sectors and are 
+# Less likely to be correlated due to being in different sectors and are
 # exposed to different market forces, economic trends, and price risks.
-#symbols = ["TSLA", "PFE", "XOM", "HD", "JPM", "AAPL", "KO", "UNH", "LMT", "AMZN"]
+# symbols = ["TSLA", "PFE", "XOM", "HD", "JPM", "AAPL", "KO", "UNH", "LMT", "AMZN"]
 
 # Here we have two groups, one with 5 technology stocks and another with 5 oil
-# stocks. These two groups are likely to be highly correlated within their 
+# stocks. These two groups are likely to be highly correlated within their
 # respective sectors but are expected to be less correlated between sectors.
-#symbols = ["AAPL", "MSFT", "GOOG", "ADBE", "CRM", "XOM", "CVX", "COP", "PSX", "OXY"]
+# symbols = ["AAPL", "MSFT", "GOOG", "ADBE", "CRM", "XOM", "CVX", "COP", "PSX", "OXY"]
 
-# Likely to be highly correlated due to being in the technology sector, 
+# Likely to be highly correlated due to being in the technology sector,
 # specifically in the sub-industry of Semiconductors:
 symbols = ["INTC", "AMD", "NVDA", "TXN", "QCOM", "MU", "AVGO", "ADI", "MCHP", "NXPI"]
 
 # Date range you are interested in
 start_date = "2022-04-01"
 end_date = "2023-05-10"
+
 
 def fetch_stock_data(symbols, start_date, end_date):
     stocks = []
@@ -75,18 +76,18 @@ def fetch_stock_data(symbols, start_date, end_date):
                 start_date,
                 end_date,
             )
-            df = pd.DataFrame(aggs, columns=['timestamp', 'close'])
+            df = pd.DataFrame(aggs, columns=["timestamp", "close"])
 
             # Filter out rows with invalid timestamps
-            df = df[df['timestamp'] > 0]
+            df = df[df["timestamp"] > 0]
 
-            df['timestamp'] = pd.to_datetime(df['timestamp'], unit='ms')
-            df.set_index('timestamp', inplace=True)
+            df["timestamp"] = pd.to_datetime(df["timestamp"], unit="ms")
+            df.set_index("timestamp", inplace=True)
 
             # Remove duplicate index values (dates)
-            df = df.loc[~df.index.duplicated(keep='first')]
+            df = df.loc[~df.index.duplicated(keep="first")]
 
-            df.rename(columns={'close': symbol}, inplace=True)
+            df.rename(columns={"close": symbol}, inplace=True)
             stocks.append(df)
     finally:
         pass
@@ -94,21 +95,34 @@ def fetch_stock_data(symbols, start_date, end_date):
     merged_stocks = pd.concat(stocks, axis=1)
     return merged_stocks
 
+
 def calculate_daily_returns(stock_data):
     daily_returns = stock_data.pct_change().dropna()
     return daily_returns
+
 
 def compute_correlation_matrix(daily_returns):
     correlation_matrix = daily_returns.corr()
     return correlation_matrix
 
+
 def plot_correlation_heatmap(correlation_matrix):
     plt.figure(figsize=(8, 8))
-    ax = sns.heatmap(correlation_matrix, annot=True, cmap='coolwarm', vmin=-1, vmax=1, square=True, linewidths=0.5, cbar_kws={"shrink": 0.8})
+    ax = sns.heatmap(
+        correlation_matrix,
+        annot=True,
+        cmap="coolwarm",
+        vmin=-1,
+        vmax=1,
+        square=True,
+        linewidths=0.5,
+        cbar_kws={"shrink": 0.8},
+    )
     ax.xaxis.tick_top()
-    ax.xaxis.set_label_position('top')
-    plt.title('Correlation Matrix Heatmap', y=1.08)
+    ax.xaxis.set_label_position("top")
+    plt.title("Correlation Matrix Heatmap", y=1.08)
     plt.show()
+
 
 def main():
     stock_data = fetch_stock_data(symbols, start_date, end_date)
@@ -120,6 +134,6 @@ def main():
 
     plot_correlation_heatmap(correlation_matrix)
 
+
 if __name__ == "__main__":
     main()
-
