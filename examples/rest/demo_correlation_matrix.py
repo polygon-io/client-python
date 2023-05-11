641e03be6f77962997690864f70bfd3da496f1a9
justinpolygon <123573436+justinpolygon@users.noreply.github.com>
2023-05-11 01:00:09 -0700

    Fix formatting

--- examples/rest/demo_correlation_matrix.py
+++ examples/rest/demo_correlation_matrix.py
@@ -37,10 +37,10 @@ as it can help manage risk and inform investment strategies. It's always
 essential to do your own research or consult a financial advisor for 
 personalized advice when investing.
 """
-import pandas as pd # type: ignore
-import numpy as np # type: ignore
-import seaborn as sns # type: ignore
-import matplotlib.pyplot as plt # type: ignore
+import pandas as pd  # type: ignore
+import numpy as np  # type: ignore
+import seaborn as sns  # type: ignore
+import matplotlib.pyplot as plt  # type: ignore
 from polygon import RESTClient
 
 # Less likely to be correlated due to being in different sectors and are
