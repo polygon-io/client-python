3f77fe6c6693fbc6c94a3bf99b6e54abeaeac0c2
Gavin Golden <gavingolden@users.noreply.github.com>
2023-05-17 15:31:24 -0500

    rename

--- examples/rest/universal-snapshot.py
+++ examples/rest/universal-snapshot.py
@@ -1,4 +1,6 @@
-from typing import cast, Iterator
+from typing import cast, Iterator, Union
+
+from urllib3 import HTTPResponse
 
 from polygon import RESTClient
 from polygon.rest.models import UniversalSnapshot, SnapshotMarketType
@@ -11,7 +13,7 @@ from polygon.rest.models import UniversalSnapshot, SnapshotMarketType
 client = RESTClient()  # POLYGON_API_KEY environment variable is used
 
 
-def print_snapshots(iterator: Iterator[UniversalSnapshot]):
+def print_snapshots(iterator: Union[Iterator[UniversalSnapshot], HTTPResponse]):
     snapshots = [s for s in iterator]
 
     print(f"count: {len(snapshots)}")
@@ -25,12 +27,22 @@ def print_snapshots(iterator: Iterator[UniversalSnapshot]):
 # it = client.list_asset_snapshots() # all tickers for all assets types in lexicographical order
 
 it = client.list_asset_snapshots(
-    ticker_any_of=["AAPL", "O:AAPL230519C00055000", "DOES_NOT_EXIST", "X:1INCHUSD", "C:AEDAUD"]
+    ticker_any_of=[
+        "AAPL",
+        "O:AAPL230519C00055000",
+        "DOES_NOT_EXIST",
+        "X:1INCHUSD",
+        "C:AEDAUD",
+    ]
 )
 print_snapshots(it)
 
-it = client.list_asset_snapshots(market_type=SnapshotMarketType.STOCKS, ticker_gt="A", ticker_lt="AAPL")
+it = client.list_asset_snapshots(
+    market_type=SnapshotMarketType.STOCKS, ticker_gt="A", ticker_lt="AAPL"
+)
 print_snapshots(it)
 
-it = client.list_asset_snapshots(market_type=SnapshotMarketType.STOCKS, ticker_gte="AAPL", ticker_lte="ABB")
+it = client.list_asset_snapshots(
+    market_type=SnapshotMarketType.STOCKS, ticker_gte="AAPL", ticker_lte="ABB"
+)
 print_snapshots(it)
