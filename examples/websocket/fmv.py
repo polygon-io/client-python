ae03944436d81f4f80282ea6cda20dbfacc7e9d1
justinpolygon <123573436+justinpolygon@users.noreply.github.com>
2023-11-27 09:34:02 -0800

    Fix FMV ws model and added example" (#566)
    * Fix FMV ws model and add example
    
    * Updated example with correct tickers

---
+++ examples/websocket/fmv.py
@@ -0,0 +1,19 @@
+from polygon import WebSocketClient
+from polygon.websocket.models import WebSocketMessage, Feed, Market
+from typing import List
+
+client = WebSocketClient(feed=Feed.Business, market=Market.Stocks, verbose=True)
+
+# FMV
+client.subscribe("FMV.*")  # all ticker symbols
+# client.subscribe("FMV.TSLA")
+# client.subscribe("FMV.AAPL")
+# client.subscribe("FMV.NVDA")
+
+
+def handle_msg(msgs: List[WebSocketMessage]):
+    for m in msgs:
+        print(m)
+
+
+client.run(handle_msg)
