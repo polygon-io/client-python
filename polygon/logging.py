189a10abc684e1c8742c6cfa25accefad577b7d4
manikpandita23 <115569552+manikpandita23@users.noreply.github.com>
2024-02-27 23:41:16 +0530

    git test

--- polygon/logging.py
+++ polygon/logging.py
@@ -8,5 +8,4 @@ def get_logger(name: str) -> logging.Logger:
     formatter = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s")
     handler.setFormatter(formatter)
     logger.addHandler(handler)
-
     return logger
