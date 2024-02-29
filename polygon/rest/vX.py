49979ba15f432656b46829313cb6d186b990c66e
manikpandita23 <115569552+manikpandita23@users.noreply.github.com>
2024-02-29 22:42:08 +0530

    unused

--- polygon/rest/vX.py
+++ polygon/rest/vX.py
@@ -34,33 +34,7 @@ class VXClient(BaseClient):
         raw: bool = False,
         options: Optional[RequestOptionBuilder] = None,
     ) -> Union[Iterator[StockFinancial], HTTPResponse]:
-        """
-        Get historical financial data for a stock ticker.
-
-        :param ticker: Query by company ticker.
-        :param cik: Query by central index key (CIK) Number.
-        :param company_name: Query by company name.
-        :param company_name_search: Query by company name with partial-match text search.
-        :param sic: Query by standard industrial classification (SIC).
-        :param filing_date: Query by the date when the filing with financials data was filed in YYYY-MM-DD format.
-        :param filing_date_lt: filing_date less than.
-        :param filing_date_lte: filing_date less than or equal to.
-        :param filing_date_gt: filing_date greater than.
-        :param filing_date_gte: filing_date greater than or equal to.
-        :param period_of_report_date: The period of report for the period_of_report with financials data in YYYY-MM-DD format.
-        :param period_of_report_date_lt: period_of_report_date less than.
-        :param period_of_report_date_lte: period_of_report_date less than or equal to.
-        :param period_of_report_date_gt: period_of_report_date greater than.
-        :param period_of_report_date_gte: period_of_report_date greater than or equal to.
-        :param timeframe: Query by timeframe.
-        :param include_sources: Whether or not to include the xpath and formula attributes for each financial data point.
-        :param limit: Limit the number of results returned per-page, default is 10 and max is 100.
-        :param sort: Sort field used for ordering.
-        :param order: Order results based on the sort field.
-        :param params: Any additional query params
-        :param raw: Return raw object instead of results object
-        :return: Iterator of financials
-        """
+        
         url = "/vX/reference/financials"
 
         return self._paginate(
