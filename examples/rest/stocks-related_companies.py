9af93459e3858e43786f8351196563159dc3613c
justinpolygon <123573436+justinpolygon@users.noreply.github.com>
2024-06-18 12:26:47 -0700

    Added related companies

---
+++ examples/rest/stocks-related_companies.py
@@ -0,0 +1,14 @@
+from polygon import RESTClient
+
+# docs
+# https://polygon.io/docs/stocks/get_v1_related-companies__ticker
+
+# client = RESTClient("XXXXXX") # hardcoded api_key is used
+client = RESTClient()  # POLYGON_API_KEY environment variable is used
+
+related_companies = client.get_related_companies("AAPL")
+for company in related_companies:
+    print(company.ticker)
+
+
+
