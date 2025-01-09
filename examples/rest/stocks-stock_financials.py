bbf0b29b7ab9cc207f89fa94f36ba7facbb6f504
justinpolygon <123573436+justinpolygon@users.noreply.github.com>
2025-01-09 08:03:02 -0800

    Update StockFinancial model (net income, EPS, and parent net income loss) (#817)
    * Update StockFinancial model (net income, EPS, and parent net income loss)
    
    * Fix test
    
    * Patch json to remove other_than_fixed_noncurrent_assets
    
    * Removed top level values to match spec
    
    * Updated StockFinancial to remove nested values
    
    * Update comment
    
    * Remove vX experimental test
    
    * Refactored to use typed classes and explicit fields rather than raw dictionaries.
    
    * Fix lint

--- examples/rest/stocks-stock_financials.py
+++ examples/rest/stocks-stock_financials.py
@@ -8,6 +8,13 @@ from polygon import RESTClient
 client = RESTClient()  # POLYGON_API_KEY environment variable is used
 
 financials = []
-for f in client.vx.list_stock_financials("AAPL"):
+for f in client.vx.list_stock_financials("AAPL", filing_date="2024-11-01"):
     financials.append(f)
+
+    # get diluted_earnings_per_share
+    # print(f.financials.income_statement.diluted_earnings_per_share)
+
+    # get net_income_loss
+    # print(f.financials.income_statement.net_income_loss)
+
 print(financials)
