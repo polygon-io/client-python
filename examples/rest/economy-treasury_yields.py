3452aa1edc1a71e5e42060fa783a2df50c2e971b
justinpolygon <123573436+justinpolygon@users.noreply.github.com>
2025-04-23 15:20:51 -0700

    Adds IPOs, Short Interest/Volume, and Treasury Yields

--- examples/rest/economy-treasury_yields.py
+++ examples/rest/economy-treasury_yields.py
@@ -1,13 +1,13 @@
 from polygon import RESTClient
 
 # docs
-# https://polygon.io/docs/rest/stocks/corporate-actions/ipos
+# https://staging.polygon.io/docs/rest/economy/treasury-yields
 
 # client = RESTClient("XXXXXX") # hardcoded api_key is used
 client = RESTClient()  # POLYGON_API_KEY environment variable is used
 
-ipos = []
-for ipo in client.vx.list_ipos(ticker="RDDT"):
-    ipos.append(ipo)
+yields = []
+for date in client.vx.list_treasury_yields():
+    yields.append(date)
 
-print(ipos)
+print(yields)
--- examples/rest/stocks-short_volume.py
+++ examples/rest/stocks-short_volume.py
@@ -1,13 +1,13 @@
 from polygon import RESTClient
 
 # docs
-# https://polygon.io/docs/rest/stocks/corporate-actions/ipos
+# https://polygon.io/docs/rest/stocks/fundamentals/short-volume
 
 # client = RESTClient("XXXXXX") # hardcoded api_key is used
 client = RESTClient()  # POLYGON_API_KEY environment variable is used
 
-ipos = []
-for ipo in client.vx.list_ipos(ticker="RDDT"):
-    ipos.append(ipo)
+items = []
+for item in client.vx.list_short_volume(ticker="RDDT"):
+    items.append(item)
 
-print(ipos)
+print(items)
